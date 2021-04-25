91d23b9d7b22fea055efadc6126ea49ad6cdf5ad
Johnny Starr <johnnystarr@gmail.com>
2021-04-25 15:49:25 -0500

    seeking 100% coverage

--- src/main/kotlin/io/johnnystarr/jvm65/Main.kt
+++ src/main/kotlin/io/johnnystarr/jvm65/Main.kt
@@ -1,13 +1,5 @@
 package io.johnnystarr.jvm65
 
-import io.johnnystarr.jvm65.core.UnsignedByte
-
 fun main(args: Array<String>) {
-    println("JVM65!")
-
-    var byte = UnsignedByte(1)
-    byte += 230
-
-    println("The byte value is $byte")
-
+    println("JVM65.")
 }
