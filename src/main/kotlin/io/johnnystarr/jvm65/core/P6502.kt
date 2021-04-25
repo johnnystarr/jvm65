e2f97b456a4e2a6cd06e3070d1deb9a3e39ef158
Johnny Starr <johnnystarr@gmail.com>
2021-04-24 19:25:56 -0500

    trying scan

--- src/main/kotlin/io/johnnystarr/jvm65/core/P6502.kt
+++ src/main/kotlin/io/johnnystarr/jvm65/core/P6502.kt
@@ -1,11 +1,11 @@
 package io.johnnystarr.jvm65.core
 
 class P6502() : Processor {
-    var a = UnsignedByte(0) // A Register
-    var x = UnsignedByte(0) // X Register
-    var y = UnsignedByte(0) // Y Register
-    var sr: Short = 0  // Status Register
-    var pc: Short = 0  // Program Counter
+    var a = UnsignedByte(0)  // A Register
+    var x = UnsignedByte(0)  // X Register
+    var y = UnsignedByte(0)  // Y Register
+    var sr = UnsignedWord(0) // Status Register
+    var pc = UnsignedWord(0) // Program Counter
 
     // flags
     var carryFlag = false
--- src/test/kotlin/io/johnnystarr/jvm65/core/P6502Test.kt
+++ src/test/kotlin/io/johnnystarr/jvm65/core/P6502Test.kt
@@ -1,14 +1,28 @@
 package io.johnnystarr.jvm65.core
 
-import org.junit.jupiter.api.AfterEach
+import org.junit.jupiter.api.Assertions.*
 import org.junit.jupiter.api.BeforeEach
 import org.junit.jupiter.api.Test
 
 internal class P6502Test {
-    private lateinit var cpu: Processor
+    private lateinit var cpu: P6502
 
     @BeforeEach
     fun setUp() {
         cpu = P6502()
+    }
+
+    @Test
+    fun `P6502 default values`() {
+        assertFalse(cpu.carryFlag)
+        assertFalse(cpu.zeroFlag)
+        assertFalse(cpu.decimalFlag)
+        assertFalse(cpu.overflowFlag)
+        assertFalse(cpu.negativeFlag)
+        assertEquals(0, cpu.a.value)
+        assertEquals(0, cpu.x.value)
+        assertEquals(0, cpu.y.value)
+        assertEquals(0, cpu.sr.value)
+        assertEquals(0, cpu.pc.value)
     }
 }
