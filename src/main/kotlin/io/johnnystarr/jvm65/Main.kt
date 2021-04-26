44b3b29b7a41554896767ba032792b20a0080102
Johnny Starr <johnnystarr@gmail.com>
2021-04-26 17:40:17 -0500

    adding encrypted signing

--- src/main/kotlin/io/johnnystarr/jvm65/Main.kt
+++ src/main/kotlin/io/johnnystarr/jvm65/Main.kt
@@ -1,10 +1,5 @@
 package io.johnnystarr.jvm65
 
-import io.johnnystarr.jvm65.core.MemoryManager
-
 fun main(args: Array<String>) {
-    val map = MemoryManager(0xFFFF)
-    map.memory[0x0A]?.value = 13
-
-    println(map.memory[0x0A])
+    println("jvm65")
 }
