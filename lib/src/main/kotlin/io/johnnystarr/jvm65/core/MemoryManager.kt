deda4934a8da0e3ad81239e1dcebcf0437cda670
Johnny Starr <johnnystarr@gmail.com>
2021-04-28 19:44:03 -0500

    refactor: MMU and MemoryManager

--- lib/src/main/kotlin/io/johnnystarr/jvm65/core/MemoryManager.kt
+++ lib/src/main/kotlin/io/johnnystarr/jvm65/core/MemoryManager.kt
@@ -5,58 +5,13 @@
 package io.johnnystarr.jvm65.core
 
 /**
- * Class for Memory Manager
- *
- * @constructor Creates a new Memory Manager
- * @param size [Int] The total size of memory in bytes
- * @param cpu [P6502] A reference to the Processor
+ * Memory Management Interface
  */
-class MemoryManager(size: Int, var cpu: P6502) : MMU {
-    override var memory: HashMap<Int, UnsignedByte> = HashMap()
+interface MemoryManager {
+    var memory: HashMap<Int, UnsignedByte>
 
-    init {
-        for (i in 0..size) {
-            this.memory[i] = UnsignedByte(0)
-        }
-    }
-
-    /**
-     * Retrieve a byte from a memory address
-     * @param index [Int] the index/offset address
-     * @return [UnsignedByte] returns the byte at this address
-     */
-    override fun at(index: Int): UnsignedByte? {
-        return this.memory[index]
-    }
-
-    /**
-     * Retrieve a byte from a memory address + the X register
-     * @param index [Int] the index/offset address
-     * @return [UnsignedByte] returns the byte at this address
-     */
-    override fun atX(index: Int): UnsignedByte? {
-        val x = this.cpu.x.value
-        return this.at(index + x)
-    }
-
-    /**
-     * Retrieve a byte from a memory address + the Y register
-     * @param index [Int] the index/offset address
-     * @return [UnsignedByte] returns the byte at this address
-     */
-    override fun atY(index: Int): UnsignedByte? {
-        val y = this.cpu.y.value
-        return this.at(index + y)
-    }
-
-    /**
-     * Insert a byte into a memory address
-     * @param address [Int] the absolute memory address
-     * @param byte [Register] the element to put in memory
-     * @return [Unit]
-     */
-    override fun put(address: Int, byte: Register) {
-        if (byte is UnsignedByte)
-            this.memory[address] = byte
-    }
+    fun at(index: Int): Register?
+    fun atX(index: Int): Register?
+    fun atY(index: Int): Register?
+    fun put(address: Int, byte: Register)
 }
