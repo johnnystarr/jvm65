aaef0d382828e8bf915cce8f0d55a87414183491
Johnny Starr <johnnystarr@gmail.com>
2021-04-29 12:50:09 -0500

    adding adc methods

--- lib/src/test/kotlin/io/johnnystarr/jvm65/core/InstructionSetTest.kt
+++ lib/src/test/kotlin/io/johnnystarr/jvm65/core/InstructionSetTest.kt
@@ -28,4 +28,44 @@ internal class InstructionSetTest {
         cpu.execute(UnsignedByte(0x69))
         assertEquals(3, cpu.a.value)
     }
+
+    @Test
+    fun `0x65 adc 1 + $00 zeropage`() {
+        cpu.a.value = 1
+        cpu.mmu.put(0, UnsignedByte(0x0A))
+        cpu.mmu.put(0x0A, UnsignedByte(1))
+        cpu.execute(UnsignedByte(0x65))
+        assertEquals(2, cpu.a.value)
+    }
+
+    @Test
+    fun `0x65 adc 1 + $00 + carry zeropage`() {
+        cpu.a.value = 1
+        cpu.carryFlag = true
+        cpu.mmu.put(0, UnsignedByte(0x0A))
+        cpu.mmu.put(0x0A, UnsignedByte(1))
+        cpu.execute(UnsignedByte(0x65))
+        assertEquals(3, cpu.a.value)
+    }
+
+    @Test
+    fun `0x75 adc 1 + $00,X zeropage`() {
+        cpu.a.value = 1
+        cpu.x.value = 1
+        cpu.mmu.put(0, UnsignedByte(0x0A))
+        cpu.mmu.put(0x0B, UnsignedByte(1))
+        cpu.execute(UnsignedByte(0x75))
+        assertEquals(2, cpu.a.value)
+    }
+
+    @Test
+    fun `0x75 adc 1 + $00,X + carry zeropage`() {
+        cpu.a.value = 1
+        cpu.x.value = 1
+        cpu.carryFlag = true
+        cpu.mmu.put(0, UnsignedByte(0x0A))
+        cpu.mmu.put(0x0B, UnsignedByte(1))
+        cpu.execute(UnsignedByte(0x75))
+        assertEquals(3, cpu.a.value)
+    }
 }
