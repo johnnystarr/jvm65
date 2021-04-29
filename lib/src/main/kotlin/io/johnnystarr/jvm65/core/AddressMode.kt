e45a4802d65ed410c7745331af939cbd2f919664
Johnny Starr <johnnystarr@gmail.com>
2021-04-28 20:56:11 -0500

    adding address mode enum

---
+++ lib/src/main/kotlin/io/johnnystarr/jvm65/core/AddressMode.kt
@@ -0,0 +1,13 @@
+/**
+ * @author Johnny Starr <johnnystarr@gmail.com>
+ */
+
+package io.johnnystarr.jvm65.core
+
+/**
+ * Address Modes
+ */
+enum class AddressMode {
+    ACCUMULATOR, ABSOLUTE, ABSOLUTE_X, ABSOLUTE_Y, IMPLIED, INDIRECT,
+    INDIRECT_X, INDIRECT_Y, RELATIVE, ZEROPAGE, ZEROPAGE_X, ZEROPAGE_Y
+}
