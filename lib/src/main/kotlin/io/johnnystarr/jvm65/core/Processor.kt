adb642e15e98b1dad52183eb7dca18b0b9a57aa9
Johnny Starr <johnnystarr@gmail.com>
2021-04-28 22:04:19 -0500

    adding boiler plate for ADC

--- lib/src/main/kotlin/io/johnnystarr/jvm65/core/Processor.kt
+++ lib/src/main/kotlin/io/johnnystarr/jvm65/core/Processor.kt
@@ -8,7 +8,7 @@ package io.johnnystarr.jvm65.core
  * Processor Interface to allow extensions
  */
 interface Processor {
-    fun execute(): Boolean
+    fun execute(opcode: UnsignedByte)
     fun status(): Register
     fun step(): Register?
     fun peek(): Register?
