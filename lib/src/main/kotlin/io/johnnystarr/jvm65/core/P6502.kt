5be7a6483813f8a4e1de1b5fece718bbc75695ab
Johnny Starr <johnnystarr@gmail.com>
2021-04-28 21:42:32 -0500

    adding basic instruction set

--- lib/src/main/kotlin/io/johnnystarr/jvm65/core/P6502.kt
+++ lib/src/main/kotlin/io/johnnystarr/jvm65/core/P6502.kt
@@ -9,7 +9,7 @@ package io.johnnystarr.jvm65.core
  *
  * @constructor Creates a P6502 Object
  */
-class P6502() : Processor {
+class P6502() : Processor, InstructionSet {
     var a = UnsignedByte(0)  // A Register
     var x = UnsignedByte(0)  // X Register
     var y = UnsignedByte(0)  // Y Register
@@ -70,5 +70,397 @@ class P6502() : Processor {
      */
     override fun peek(): UnsignedByte? {
         return this.mmu.at(this.pc.value + 1)
+    }
+
+    /**
+     * Add with carry
+     */
+    override fun adc(mode: AddressMode) {
+        // implement
+    }
+
+    /**
+     * And
+     */
+    override fun and(mode: AddressMode) {
+        // implement
+    }
+
+    /**
+     * Arithmetic shift left
+     */
+    override fun asl(mode: AddressMode) {
+        // implement
+    }
+
+    /**
+     * Branch on carry clear
+     */
+    override fun bcc(mode: AddressMode) {
+        // implement
+    }
+
+    /**
+     * Branch on carry set
+     */
+    override fun bcs(mode: AddressMode) {
+        // implement
+    }
+
+    /**
+     * Branch on equal
+     */
+    override fun beq(mode: AddressMode) {
+        // implement
+    }
+
+    /**
+     * Bit test
+     */
+    override fun bit(mode: AddressMode) {
+        // implement
+    }
+
+    /**
+     * Branch on minus
+     */
+    override fun bmi(mode: AddressMode) {
+        // implement
+    }
+
+    /**
+     * Branch on not equal
+     */
+    override fun bne(mode: AddressMode) {
+        // implement
+    }
+
+    /**
+     * Branch on plus
+     */
+    override fun bpl(mode: AddressMode) {
+        // implement
+    }
+
+    /**
+     * Break / interrupt
+     */
+    override fun brk(mode: AddressMode) {
+        // implement
+    }
+
+    /**
+     * Branch on overflow clear
+     */
+    override fun bvc(mode: AddressMode) {
+        // implement
+    }
+
+    /**
+     * Branch on overflow set
+     */
+    override fun bvs(mode: AddressMode) {
+        // implement
+    }
+
+    /**
+     * Clear carry
+     */
+    override fun clc(mode: AddressMode) {
+        // implement
+    }
+
+    /**
+     * Clear decimal
+     */
+    override fun cld(mode: AddressMode) {
+        // implement
+    }
+
+    /**
+     * Clear interrupt disable
+     */
+    override fun cli(mode: AddressMode) {
+        // implement
+    }
+
+    /**
+     * Clear overflow
+     */
+    override fun clv(mode: AddressMode) {
+        // implement
+    }
+
+    /**
+     * Compare with accumulator
+     */
+    override fun cmp(mode: AddressMode) {
+        // implement
+    }
+
+    /**
+     * Compare with X
+     */
+    override fun cpx(mode: AddressMode) {
+        // implement
+    }
+
+    /**
+     * Compare with Y
+     */
+    override fun cpy(mode: AddressMode) {
+        // implement
+    }
+
+    /**
+     * Decrement
+     */
+    override fun dec(mode: AddressMode) {
+        // implement
+    }
+
+    /**
+     * Decrement X
+     */
+    override fun dex(mode: AddressMode) {
+        // implement
+    }
+
+    /**
+     * Decrement Y
+     */
+    override fun dey(mode: AddressMode) {
+        // implement
+    }
+
+    /**
+     * Exclusive or
+     */
+    override fun eor(mode: AddressMode) {
+        // implement
+    }
+
+    /**
+     * Increment
+     */
+    override fun inc(mode: AddressMode) {
+        // implement
+    }
+
+    /**
+     * Increment X
+     */
+    override fun inx(mode: AddressMode) {
+        // implement
+    }
+
+    /**
+     * Increment Y
+     */
+    override fun iny(mode: AddressMode) {
+        // implement
+    }
+
+    /**
+     * Jump
+     */
+    override fun jmp(mode: AddressMode) {
+        // implement
+    }
+
+    /**
+     * Jump subroutine
+     */
+    override fun jsr(mode: AddressMode) {
+        // implement
+    }
+
+    /**
+     * Load accumulator
+     */
+    override fun lda(mode: AddressMode) {
+        // implement
+    }
+
+    /**
+     * Load X
+     */
+    override fun ldx(mode: AddressMode) {
+        // implement
+    }
+
+    /**
+     * Load Y
+     */
+    override fun ldy(mode: AddressMode) {
+        // implement
+    }
+
+    /**
+     * Logical shift right
+     */
+    override fun lsr(mode: AddressMode) {
+        // implement
+    }
+
+    /**
+     * No operation
+     */
+    override fun nop(mode: AddressMode) {
+        // implement
+    }
+
+    /**
+     * Or with accumulator
+     */
+    override fun ora(mode: AddressMode) {
+        // implement
+    }
+
+    /**
+     * Push accumulator onto stack
+     */
+    override fun pha(mode: AddressMode) {
+        // implement
+    }
+
+    /**
+     * Push processor status onto stack
+     */
+    override fun php(mode: AddressMode) {
+        // implement
+    }
+
+    /**
+     * Pull accumulator from stack
+     */
+    override fun pla(mode: AddressMode) {
+        // implement
+    }
+
+    /**
+     * Pull processor status from stack
+     */
+    override fun plp(mode: AddressMode) {
+        // implement
+    }
+
+    /**
+     * Rotate left
+     */
+    override fun rol(mode: AddressMode) {
+        // implement
+    }
+
+    /**
+     * Rotate right
+     */
+    override fun ror(mode: AddressMode) {
+        // implement
+    }
+
+    /**
+     * Return from interrupt
+     */
+    override fun rti(mode: AddressMode) {
+        // implement
+    }
+
+    /**
+     * Return from subroutine
+     */
+    override fun rts(mode: AddressMode) {
+        // implement
+    }
+
+    /**
+     * Subtract with carry
+     */
+    override fun sbc(mode: AddressMode) {
+        // implement
+    }
+
+    /**
+     * Set carry
+     */
+    override fun sec(mode: AddressMode) {
+        // implement
+    }
+
+    /**
+     * Set decimal
+     */
+    override fun sed(mode: AddressMode) {
+        // implement
+    }
+
+    /**
+     * Set interrupt disable
+     */
+    override fun sei(mode: AddressMode) {
+        // implement
+    }
+
+    /**
+     * Store accumulator
+     */
+    override fun sta(mode: AddressMode) {
+        // implement
+    }
+
+    /**
+     * Store X
+     */
+    override fun stx(mode: AddressMode) {
+        // implement
+    }
+
+    /**
+     * Store Y
+     */
+    override fun sty(mode: AddressMode) {
+        // implement
+    }
+
+    /**
+     * Transfer accumulator to X
+     */
+    override fun tax(mode: AddressMode) {
+        // implement
+    }
+
+    /**
+     * Transfer accumulator to Y
+     */
+    override fun tay(mode: AddressMode) {
+        // implement
+    }
+
+    /**
+     * Transfer stack pointer to X
+     */
+    override fun tsx(mode: AddressMode) {
+        // implement
+    }
+
+    /**
+     * Transfer X to accumulator
+     */
+    override fun txa(mode: AddressMode) {
+        // implement
+    }
+
+    /**
+     * Transfer X to stack pointer
+     */
+    override fun txs(mode: AddressMode) {
+        // implement
+    }
+
+    /**
+     * Transfer Y to accumulator
+     */
+    override fun tya(mode: AddressMode) {
+        // implement
     }
 }
